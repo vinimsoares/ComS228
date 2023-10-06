7ea991112168022dc1eb098f8ebe9a952183e82e
vinimsoares <116849486+vinimsoares@users.noreply.github.com>
2023-10-05 23:13:44 -0500

    Update MergeSorter.java

--- MergeSorter.java
+++ MergeSorter.java
@@ -7,14 +7,15 @@ import java.util.InputMismatchException;
 
 /**
  *  
- * @author
+ * @author Vinicius Malaman Soares 
+
  *
  */
 
 /**
  * 
  * This class implements the mergesort algorithm.   
- *
+ *@author Vinicius Malaman Soares 
  */
 
 public class MergeSorter extends AbstractSorter
@@ -26,21 +27,23 @@ public class MergeSorter extends AbstractSorter
 	 * set the instance variables algorithm in the superclass.
 	 *  
 	 * @param pts   input array of integers
+	 * @author Vinicius Malaman Soares 
 	 */
 	public MergeSorter(Point[] pts) 
 	{
-		// TODO  
+		super(pts);
+		algorithm = "MergeSort";
 	}
 
 
 	/**
 	 * Perform mergesort on the array points[] of the parent class AbstractSorter. 
-	 * 
+	 * @author Vinicius Malaman Soares 
 	 */
 	@Override 
 	public void sort()
 	{
-		// TODO 
+		mergeSortRec(points); 
 	}
 
 	
@@ -50,13 +53,60 @@ public class MergeSorter extends AbstractSorter
 	 * and merge the two sorted subarrays into pts[].   
 	 * 
 	 * @param pts	point array 
+	 * @author Vinicius Malaman Soares 
 	 */
 	private void mergeSortRec(Point[] pts)
 	{
+		int size = pts.length;
+		if(size <= 1) {
+		    return;
+		}else {
+		int half = size/2;
+		Point[] leftSide = new Point[half];
+		Point[] rightSide = new Point[size - half];
 		
+		for(int i =0; i< half; i++) {
+		    leftSide[i] = pts[i];
+		}
+		for(int i = 0;i<rightSide.length;i++ ) {
+		    rightSide[i] = pts[i+half];
+		}
+		
+		mergeSortRec(leftSide);
+		mergeSortRec(rightSide);
+		
+		merge(pts,leftSide,rightSide);
+		}
 	}
 
 	
 	// Other private methods if needed ...
+	private void merge(Point[] arr, Point[] left, Point[] right) {
+	    int leftMid = left.length;
+	    int rightMid = right.length;
+	    int i =0;
+	    int k = 0;
+	    int j = 0;
+	    while(i<leftMid && j <rightMid) {
+		if(pointComparator.compare(left[i], right[j])<0) {
+		    arr[k] = left[i];
+		    i++;
+		}else {
+		    arr[k] = right[j];
+		    j++;
+		}
+		k++;
+	    }
+	    while(i< leftMid) {
+		arr[k] = left[i];
+		i++;
+		k++;
+	    }
+	    while(j < rightMid) {
+		arr[k] = right[j];
+		j++;
+		k++;
+	    }
+	}
 
 }
