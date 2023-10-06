b28661d86c1b5ca8c60449a49cce77998212158b
vinimsoares <116849486+vinimsoares@users.noreply.github.com>
2023-10-05 23:12:34 -0500

    Update AbstractSorter.java

--- AbstractSorter.java
+++ AbstractSorter.java
@@ -2,7 +2,7 @@ package edu.iastate.cs228.hw2;
 
 /**
  *  
- * @author
+ * @author Vinicius Malaman Soares 
  *
  */
 
@@ -74,9 +74,33 @@ public abstract class AbstractSorter
 	public void setComparator(int order) throws IllegalArgumentException
 	{
 		if(order == 0) {
-		
+		    Point.setXorY(true);
+		    pointComparator = new Comparator<Point>() {
+			@Override
+			public int compare(Point p1, Point p2) {
+    			    if(p1.getX()<p2.getX()) {
+				return -1;
+			    }else if(p1.getX()== p2.getX()) {
+				return 0;
+			    }else {
+				return 1;
+			    }
+			}
+		    };
 		}else if(order == 1) {
-		    
+		    Point.setXorY(false);
+		    pointComparator = new Comparator<Point>() {
+			@Override
+			public int compare(Point p1, Point p2) {
+			    if(p1.getY()<p2.getY()) {
+				return -1;
+			    }else if(p1.getY()== p2.getY()) {
+				return 0;
+			    }else {
+				return 1;
+			    }
+			}
+		    };
 		}else {
 		    throw new IllegalArgumentException();
 		}
