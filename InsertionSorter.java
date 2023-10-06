2ad8692d8ad699e67cc29434ac734765326fb8f2
vinimsoares <116849486+vinimsoares@users.noreply.github.com>
2023-10-05 23:13:25 -0500

    Update InsertionSorter.java

--- InsertionSorter.java
+++ InsertionSorter.java
@@ -2,20 +2,20 @@ package edu.iastate.cs228.hw2;
 
 import java.io.FileNotFoundException;
 import java.lang.NumberFormatException; 
-import java.lang.IllegalArgumentException; 
+import java.lang.IllegalArgumentException;
+import java.util.Arrays;
 import java.util.InputMismatchException;
 
 
 /**
  *  
- * @author
- *
+ * @author Vinicius Malaman Soares 
  */
 
 /**
  * 
  * This class implements insertion sort.   
- *
+ *@author Vinicius Malaman Soares 
  */
 
 public class InsertionSorter extends AbstractSorter 
@@ -26,20 +26,33 @@ public class InsertionSorter extends AbstractSorter
 	 * Constructor takes an array of points.  It invokes the superclass constructor, and also 
 	 * set the instance variables algorithm in the superclass.
 	 * 
-	 * @param pts  
+	 * @param pts 
+	 * @author Vinicius Malaman Soares  
 	 */
 	public InsertionSorter(Point[] pts) 
 	{
-		// TODO 
+		super(pts);
+		algorithm = "InsertionSort";
 	}	
 
 	
 	/** 
-	 * Perform insertion sort on the array points[] of the parent class AbstractSorter.  
+	 * Perform insertion sort on the array points[] of the parent class AbstractSorter.
+	 * @author Vinicius Malaman Soares   
 	 */
 	@Override 
 	public void sort()
 	{
-		// TODO 
+		for(int i = 0; i< points.length;i++) {
+		    Point key = points[i];
+		    int j = i-1;
+		    
+		    while(j>-1 && pointComparator.compare(points[j], key)>0) {
+			points[j+1] = points[j];
+			j--;
+			
+		    }
+		    points[j+1] =key;
+		} 
 	}		
 }
